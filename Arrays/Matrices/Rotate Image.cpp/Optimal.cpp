b0e24bd190939f9a92077ea12f3e00bff2ace230
Jyothirmai Vadlamudi <82148890+jyothirmai27@users.noreply.github.com>
2022-01-24 08:25:27 +0530

    Update Optimal.cpp

--- Arrays/Matrices/Rotate Image.cpp/Optimal.cpp
+++ Arrays/Matrices/Rotate Image.cpp/Optimal.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+       int n = matrix.size();
+        for(int i=0;i<n;i++)
+        {
+            // j = 0; j < i; j++
+            // j = i; j < n; j++
+            for(int j = 0 ; j < i ; j++)
+            {
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+        
+    }
+};
